e529f0fcc386b12b887b8a43f035ea518783d421
Kevin <kevin.billet.pro@gmail.com>
2019-12-17 11:48:19 +0100

    add choose_name and name methods

--- characters/magician.py
+++ characters/magician.py
@@ -1,8 +1,9 @@
 class Magician:
-mahyadine
 #mana = 200
-    def __init__(self, 600, 20, 50, 25):
-        
+    def __init__(self, name):
+        super().__init__(600,20,50,25)
+    
+    def heal(self)    
 
 
 
